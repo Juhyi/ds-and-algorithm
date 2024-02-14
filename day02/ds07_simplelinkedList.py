9ce0f1ddd97ea6806946ee4966684d739b12f2c5
Juhyi <dlwngml201@naver.com>
2024-02-14 12:46:56 +0900

    오전 수업 커밋

--- day02/ds07_simplelinkedList.py
+++ day02/ds07_simplelinkedList.py
@@ -1,40 +1,77 @@
-# file : ds07_simplelinkedList.py
-# desc : 단순연결리스트 일반 구현
-
-memory = [] # 컴퓨터 메모리를 유사구성
-# head, curr, prev 일반변수
-head, curr, prev = None, None, None
-
-
-class Node():
-    # data, link 두 개의 멤버변수 존재
-    # 생성자 추가
-    def __init__(self, name) -> None:
-        self.data = name
-        self.link = None
-#  클래스 끝
-def printNodes(start):  # 클래스의 멤버함수 X
-    curr = start
-    if curr == None: return # 함수를 빠져나감
-    print(curr.data, end = ' -> ')
-    while curr.link != None:
-        curr = curr.link    #  내 노드의 다음의 노드가 curr이 됨
-        print(curr.data, end = ' -> ') # end -> 로 해서 enter가 없음
-    print() # enter 추가 
-
-dataArray = ['다현', '정연', '쯔위', '사나', '지효']
-
-# 메인 시작
-if __name__ == '__main__':
-    node = Node(dataArray[0]) # '다현' 데이터를 담은 노드 생성
-    head = node  # 첫번째 값을 head가 가리킴
-    memory.append(node) # 가짜 메모리에 집어넣음
-
-    for data in dataArray[1:]:  # 두번째 노드부터 끝까지
-        prev = node
-        node = Node(data)  # 정연
-        prev.link = node
-        memory.append(node)
-    
-    printNodes(head)
-
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# file : ds07_simplelinkedList.ipynb\n",
+    "# desc : 단순 연결 리스트 일반구현"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "## 자료구조 및 알고리즘\n",
+    "- 단순연결리스트 일반구현\n",
+    "\n",
+    "### 단순연결리스트"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# 초기화\n",
+    "memory = []\n",
+    "head, curr, prev = None, None, None\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class Node():\n",
+    "    # data, link 두 개의 멤버변수 존재\n",
+    "    # 생성자 추가\n",
+    "    def __init__(self, name) -> None:\n",
+    "        self.data = name\n",
+    "        self.link = None"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
